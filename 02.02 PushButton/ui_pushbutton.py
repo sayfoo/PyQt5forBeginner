049d1d226706df9197e8a857b0993a9aa744fea5
sayfoo <kim.sajoong@gmail.com>
2022-08-08 21:00:07 +0900

    newly edited my practice part II

---
+++ 02.02 PushButton/ui_pushbutton.py
@@ -0,0 +1,208 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'pushbuttonsWGzlr.ui'
+##
+## Created by: Qt User Interface Compiler version 6.3.1
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QDialog, QGridLayout, QHBoxLayout,
+    QLabel, QPushButton, QSizePolicy, QSpacerItem,
+    QWidget)
+
+class Ui_Dialog(object):
+    def setupUi(self, Dialog):
+        if not Dialog.objectName():
+            Dialog.setObjectName(u"Dialog")
+        Dialog.resize(565, 510)
+        self.gridLayout = QGridLayout(Dialog)
+        self.gridLayout.setObjectName(u"gridLayout")
+        self.verticalSpacer = QSpacerItem(20, 150, QSizePolicy.Minimum, QSizePolicy.Preferred)
+
+        self.gridLayout.addItem(self.verticalSpacer, 4, 0, 1, 1)
+
+        self.horizontalLayout_2 = QHBoxLayout()
+        self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
+        self.btn_03 = QPushButton(Dialog)
+        self.btn_03.setObjectName(u"btn_03")
+        self.btn_03.setMaximumSize(QSize(200, 40))
+        font = QFont()
+        font.setPointSize(14)
+        self.btn_03.setFont(font)
+        self.btn_03.setStyleSheet(u"QPushButton::hover\n"
+"{\n"
+"color: rgb(0, 0, 0);\n"
+"background-color : lightgreen;\n"
+"border-style: outset;\n"
+"border-width: 3px;\n"
+"border-radius: 15px;\n"
+"border-color: rgb(255, 0, 0);\n"
+"padding: 2px;\n"
+"}\n"
+"QPushButton\n"
+"{\n"
+"color: rgb(0, 255, 255);\n"
+"	background-color: rgb(0, 0, 255);\n"
+"border-style: outset;\n"
+"border-width: 3px;\n"
+"border-radius: 15px;\n"
+"border-color: rgb(255, 0, 0);\n"
+"padding: 2px;\n"
+"}")
+
+        self.horizontalLayout_2.addWidget(self.btn_03)
+
+        self.btn_04 = QPushButton(Dialog)
+        self.btn_04.setObjectName(u"btn_04")
+        self.btn_04.setMinimumSize(QSize(200, 40))
+        self.btn_04.setMaximumSize(QSize(200, 40))
+        self.btn_04.setFont(font)
+        self.btn_04.setStyleSheet(u"QPushButton::hover\n"
+"{\n"
+"color: rgb(0, 0, 0);\n"
+"background-color : lightgreen;\n"
+"border-style: outset;\n"
+"border-width: 3px;\n"
+"border-radius: 15px;\n"
+"border-color: rgb(255, 0, 0);\n"
+"padding: 2px;\n"
+"}\n"
+"QPushButton\n"
+"{\n"
+"color: rgb(0, 255, 255);\n"
+"background-color: rgb(255, 0, 255);\n"
+"border-style: outset;\n"
+"border-width: 3px;\n"
+"border-radius: 15px;\n"
+"border-color: rgb(255, 0, 0);\n"
+"padding: 2px;\n"
+"}")
+
+        self.horizontalLayout_2.addWidget(self.btn_04)
+
+
+        self.gridLayout.addLayout(self.horizontalLayout_2, 3, 0, 1, 1)
+
+        self.verticalSpacer_3 = QSpacerItem(20, 60, QSizePolicy.Minimum, QSizePolicy.Preferred)
+
+        self.gridLayout.addItem(self.verticalSpacer_3, 2, 0, 1, 1)
+
+        self.horizontalLayout = QHBoxLayout()
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
+        self.btn_01 = QPushButton(Dialog)
+        self.btn_01.setObjectName(u"btn_01")
+        self.btn_01.setMinimumSize(QSize(200, 40))
+        self.btn_01.setMaximumSize(QSize(200, 40))
+        self.btn_01.setFont(font)
+        self.btn_01.setStyleSheet(u"QPushButton::hover\n"
+"{\n"
+"color: rgb(0, 0, 0);\n"
+"background-color : lightgreen;\n"
+"border-style: outset;\n"
+"border-width: 3px;\n"
+"border-radius: 15px;\n"
+"border-color: rgb(255, 0, 0);\n"
+"padding: 2px;\n"
+"}\n"
+"QPushButton\n"
+"{\n"
+"color: rgb(0, 255, 255);\n"
+"background-color: rgb(170, 0, 0);\n"
+"border-style: outset;\n"
+"border-width: 3px;\n"
+"border-radius: 15px;\n"
+"border-color: rgb(255, 0, 0);\n"
+"padding: 2px;\n"
+"}")
+
+        self.horizontalLayout.addWidget(self.btn_01)
+
+        self.btn_02 = QPushButton(Dialog)
+        self.btn_02.setObjectName(u"btn_02")
+        self.btn_02.setMinimumSize(QSize(200, 40))
+        self.btn_02.setMaximumSize(QSize(200, 40))
+        self.btn_02.setFont(font)
+        self.btn_02.setStyleSheet(u"QPushButton::hover\n"
+"{\n"
+"color: rgb(0, 0, 0);\n"
+"background-color : lightgreen;\n"
+"border-style: outset;\n"
+"border-width: 3px;\n"
+"border-radius: 15px;\n"
+"border-color: rgb(255, 0, 0);\n"
+"padding: 2px;\n"
+"}\n"
+"QPushButton\n"
+"{\n"
+"color: rgb(0, 255, 255);\n"
+"	background-color: rgb(85, 85, 0);\n"
+"border-style: outset;\n"
+"border-width: 3px;\n"
+"border-radius: 15px;\n"
+"border-color: rgb(255, 0, 0);\n"
+"padding: 2px;\n"
+"}")
+
+        self.horizontalLayout.addWidget(self.btn_02)
+
+
+        self.gridLayout.addLayout(self.horizontalLayout, 1, 0, 1, 1)
+
+        self.verticalSpacer_2 = QSpacerItem(20, 40, QSizePolicy.Minimum, QSizePolicy.Expanding)
+
+        self.gridLayout.addItem(self.verticalSpacer_2, 0, 0, 1, 1)
+
+        self.label = QLabel(Dialog)
+        self.label.setObjectName(u"label")
+        self.label.setMinimumSize(QSize(0, 50))
+        font1 = QFont()
+        font1.setFamilies([u"\uad81\uc11c"])
+        font1.setPointSize(14)
+        self.label.setFont(font1)
+        self.label.setStyleSheet(u"QLabel::hover\n"
+"{\n"
+"color: rgb(0, 0, 0);\n"
+"background-color : lightgreen;\n"
+"border-style: outset;\n"
+"border-width: 3px;\n"
+"border-radius: 15px;\n"
+"border-color: rgb(255, 0, 0);\n"
+"padding: 2px;\n"
+"}\n"
+"QLabel\n"
+"{\n"
+"color: rgb(0, 255, 255);\n"
+"background-color: rgb(0, 0, 0);\n"
+"border-style: outset;\n"
+"border-width: 3px;\n"
+"border-radius: 15px;\n"
+"border-color: rgb(255, 0, 0);\n"
+"padding: 2px;\n"
+"}")
+
+        self.gridLayout.addWidget(self.label, 5, 0, 1, 1)
+
+
+        self.retranslateUi(Dialog)
+
+        QMetaObject.connectSlotsByName(Dialog)
+    # setupUi
+
+    def retranslateUi(self, Dialog):
+        Dialog.setWindowTitle(QCoreApplication.translate("Dialog", u"Dialog", None))
+        self.btn_03.setText(QCoreApplication.translate("Dialog", u"Button_03", None))
+        self.btn_04.setText(QCoreApplication.translate("Dialog", u"Button_04", None))
+        self.btn_01.setText(QCoreApplication.translate("Dialog", u"Button_01", None))
+        self.btn_02.setText(QCoreApplication.translate("Dialog", u"Button_02", None))
+        self.label.setText(QCoreApplication.translate("Dialog", u"<html><head/><body><p align=\"center\"><span style=\" font-weight:700;\">\uc5ec\uae30\uc11c \ubcf4\uc5ec\uc838\uc57c...</span></p></body></html>", None))
+    # retranslateUi
+
